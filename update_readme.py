0881eb7921d78f8b89c9b65ee918e3db87981530
rayct <rayct@icloud.com>
2025-02-06 20:20:16 +0000

    Add GitHub Actions workflow for daily README update - Updated Quote formatting

--- update_readme.py
+++ update_readme.py
@@ -3,18 +3,18 @@ import datetime
 
 # List of scientific quotes
 quotes = [
-    """```Science is a way of thinking much more than it is a body of knowledge.
-- Carl Sagan```""",
-    """```The important thing is not to stop questioning. Curiosity has its own reason for existing.
-- Albert Einstein```""",
-    """```Equipped with his five senses, man explores the universe around him and calls the adventure Science.
-- Edwin Powell Hubble```""",
-    """```Somewhere, something incredible is waiting to be known.
-- Carl Sagan```""",
-    """```Science is organized knowledge. Wisdom is organized life.
-- Immanuel Kant```""",
-    """```The good thing about science is that it’s true whether or not you believe in it.
-- Neil deGrasse Tyson```"""
+    """Science is a way of thinking much more than it is a body of knowledge.
+- Carl Sagan""",
+    """The important thing is not to stop questioning. Curiosity has its own reason for existing.
+- Albert Einstein""",
+    """Equipped with his five senses, man explores the universe around him and calls the adventure Science.
+- Edwin Powell Hubble""",
+    """Somewhere, something incredible is waiting to be known.
+- Carl Sagan""",
+    """Science is organized knowledge. Wisdom is organized life.
+- Immanuel Kant""",
+    """The good thing about science is that it’s true whether or not you believe in it.
+- Neil deGrasse Tyson"""
 ]
 
 # Join all the quotes into a single string with each quote separated by a newline
