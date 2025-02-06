2eee1f5a4d5c6d80c94af2190c8f581924380746
rayct <rayct@icloud.com>
2025-02-06 16:49:38 +0000

    Add GitHub Actions workflow for daily README update - Updated Quote formattiing

--- update_readme.py
+++ update_readme.py
@@ -24,7 +24,7 @@ quote = random.choice(quotes)
 current_date = datetime.datetime.now().strftime("%d-%m-%Y")
 
 # Update the README.md file
-readme_content = f"""# rayct Repository
+readme_content = f"""# Ray's Repository
 
 **Date:** {current_date}
 
